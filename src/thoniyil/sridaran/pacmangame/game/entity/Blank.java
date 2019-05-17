3874fc12af3acdfe7b1780a4a12135caa53a8293
srithon <36751023+srithon@users.noreply.github.com>
2019-05-17 10:39:56 -0400

    Update Blank.java

--- src/thoniyil/sridaran/pacmangame/game/entity/Blank.java
+++ src/thoniyil/sridaran/pacmangame/game/entity/Blank.java
@@ -1,7 +1,14 @@
+import javafx.scene.image.Image;
+
 public class Blank extends Entity
 {
   public Blank(int x, int y)
   {
     super(x, y);
+  }
+  
+  public Image getImage()
+  {
+    return null;
   }
 }
