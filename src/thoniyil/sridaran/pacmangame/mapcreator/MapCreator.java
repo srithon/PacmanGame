6e0ae10f017c87c1e28b9cad957cccfd6ccdac07
srithon <36751023+srithon@users.noreply.github.com>
2019-05-18 17:53:36 -0700

    Fixed MapCreator resizing

--- src/thoniyil/sridaran/pacmangame/mapcreator/MapCreator.java
+++ src/thoniyil/sridaran/pacmangame/mapcreator/MapCreator.java
@@ -6,13 +6,23 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import thoniyil.sridaran.pacmangame.game.entity.Wall;
 
@@ -20,11 +30,14 @@ public class MapCreator extends Application
 {
 	private static Map currentMap;
 	private static int tileSize = 20;
-	private static Label currentFileName;
 	private static TogglableImageView[][] icons;
+	private static Stage primaryStage;
 	
-	private static final int h = 20;
-	private static final int w = 20;
+	private static TextField widthField;
+	private static TextField heightField;
+	
+	private static int h = 20;
+	private static int w = 20;
 	
 	public static void main(String[] args)
 	{
@@ -33,17 +46,70 @@ public class MapCreator extends Application
 
 	public void start(Stage stage) throws Exception
 	{
-		GridPane pane = new GridPane();
-		initPane(pane);
-		Scene sc = new Scene(pane);
-		stage.setScene(sc);
+		primaryStage = stage;
+		Scene mapCreatorScene = getMapCreatorScene();
+		stage.setScene(mapCreatorScene);
 		stage.show();
+		stage.sizeToScene();
+		//System.out.println("(" + pane.getHeight() + ", " + pane.getHeight() + ")");
+	}
+	
+	private Scene getMapCreatorScene()
+	{
+		VBox master = new VBox();
+		HBox topBar = new HBox(10);
+		GridPane pane = new GridPane();
+		initTopBar(topBar);
+		initPane(pane);
+		master.getChildren().add(topBar);
+		master.getChildren().add(pane);
+		pane.setMaxSize(w * tileSize, h * tileSize);
+		pane.setPadding(new Insets(0, -(w * tileSize) / 4, -(h * tileSize) / 4, 0));
+		Scene sc = new Scene(master);
+		//pane.setOnMouseClicked((MouseEvent e) -> System.out.println("Scene clicked"));
+		return sc;
+	}
+	
+	private void initTopBar(HBox topBar)
+	{
+		Label width = new Label("Width");
+		Label height = new Label("Height");
+		
+		widthField = new TextField();
+		heightField = new TextField();
+		
+		widthField.setMaxWidth(40.0);
+		heightField.setMaxWidth(40.0);
+		
+		Button refreshButton = new Button("Refresh");
+		refreshButton.setOnAction((ActionEvent e) -> {
+			try
+			{
+				w = Integer.parseInt(widthField.getText());
+				h = Integer.parseInt(heightField.getText());
+				
+				if (h > 40 || w > 40)
+					tileSize = 15;
+				else if (h < 15 || w < 15)
+					tileSize = 30;
+				
+				TogglableImageView.resizeImages();
+				
+				primaryStage.setScene(getMapCreatorScene());
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		});
+		
+		topBar.getChildren().addAll(width, widthField, height, heightField, refreshButton);
 	}
 	
 	private void initPane(GridPane pane)
 	{
 		icons = new TogglableImageView[h][w];
-		for (int r = 0; r < w; r++)
+		for (int r = 0; r < h; r++)
 		{
 			RowConstraints con = new RowConstraints();
 	        // Here we set the pref height of the row, but you could also use .setPercentHeight(double) if you don't know much space you will need for each label.
@@ -54,11 +120,9 @@ public class MapCreator extends Application
 	        col.setPrefWidth(tileSize);
 	        pane.getColumnConstraints().add(col);
 	        
-			for (int c = 0; c < h; c++)
+			for (int c = 0; c < w; c++)
 			{
 				icons[r][c] = new TogglableImageView();
-				
-				icons[r][c].setImage(Wall.getStaticImage());
 				
 				GridPane.setHalignment(icons[r][c], HPos.CENTER);
 				GridPane.setValignment(icons[r][c], VPos.CENTER);
@@ -81,5 +145,10 @@ public class MapCreator extends Application
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static int getTileSize()
+	{
+		return tileSize;
 	}
 }
--- src/thoniyil/sridaran/pacmangame/mapcreator/TogglableImageView.java
+++ src/thoniyil/sridaran/pacmangame/mapcreator/TogglableImageView.java
@@ -1,11 +1,28 @@
 package thoniyil.sridaran.pacmangame.mapcreator;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
 import thoniyil.sridaran.pacmangame.game.entity.Wall;
 
 public class TogglableImageView extends ImageView
 {
+	private static Image emptyImage;
+	private static Image wallImage;
+	
+	private static short dragCount;
+	
+	private short lastDrag;
+	
+	static
+	{
+		emptyImage = new Image("file:blank.png", 20, 20, false, true);
+		wallImage = new Image("file:wall.png", 20, 20, false, true);
+		
+		dragCount = 0;
+	}
+	
 	private boolean state;
 	
 	public TogglableImageView()
@@ -17,9 +34,20 @@ public class TogglableImageView extends ImageView
 	{
 		super();
 		this.state = state;
-		this.setOnDragDetected((MouseEvent e) -> this.startFullDrag());
+		lastDrag = -1;
+		this.setOnDragDetected((MouseEvent e) -> {
+			this.startFullDrag();
+			dragCount++;
+		});
 		this.setOnMouseClicked((MouseEvent e) -> this.toggle());
-		this.setOnMouseDragOver((MouseEvent e) -> this.toggle());
+		this.setOnMouseDragOver((MouseEvent e) -> {
+			if (lastDrag != dragCount)
+			{
+				this.toggle();
+				lastDrag = dragCount;
+			}
+		});
+		this.setImage(wallImage);
 	}
 	
 	public boolean getState()
@@ -38,8 +66,14 @@ public class TogglableImageView extends ImageView
 		System.out.println("Toggled");
 		
 		if (state)
-			setImage(null);
+			setImage(emptyImage);
 		else
-			setImage(Wall.getStaticImage());
+			setImage(wallImage);
+	}
+	
+	public static void resizeImages()
+	{
+		emptyImage = new Image("file:blank.png", MapCreator.getTileSize(), MapCreator.getTileSize(), false, true);
+		wallImage = new Image("file:wall.png", MapCreator.getTileSize(), MapCreator.getTileSize(), false, true);
 	}
 }
