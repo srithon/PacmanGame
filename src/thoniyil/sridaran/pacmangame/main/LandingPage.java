93ab1535a36193a45e15fbb68e69edd4bb496388
srithon <36751023+srithon@users.noreply.github.com>
2019-05-20 16:58:37 -0700

    Functioning FXML

--- src/thoniyil/sridaran/pacmangame/main/LandingPage.java
+++ src/thoniyil/sridaran/pacmangame/main/LandingPage.java
@@ -4,10 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class LandingPage extends Application
 {
+	private static Stage stage;
+	
 	public static void main(String[] args)
 	{
 		launch(args);
@@ -16,9 +19,20 @@ public class LandingPage extends Application
 	@Override
 	public void start(Stage stage) throws Exception
 	{
+		LandingPage.stage = stage;
 		Parent root = FXMLLoader.load(getClass().getResource("LandingPageFXML.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+	}
+	
+	public static void openMapCreator()
+	{
+		openMapCreator(null);
+	}
+	
+	public static void openMapCreator(Image mapTemplate)
+	{
+		
 	}
 }
--- src/thoniyil/sridaran/pacmangame/main/LandingPageFXMLController.java
+++ src/thoniyil/sridaran/pacmangame/main/LandingPageFXMLController.java
@@ -1,19 +1,103 @@
 package thoniyil.sridaran.pacmangame.main;
 
+import java.io.File;
+import java.util.Arrays;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 public class LandingPageFXMLController
 {
+	private Image[] mapImages;
+	private File[] mapFiles;
+	
 	@FXML
     private ListView<String> mapListView;
 
-    public void initialize() {
-        // TODO
+	//called by FXML loader after done loading objects
+    public void initialize()
+    {
+        mapFiles = new File("maps/").listFiles((File i) -> i.getName().endsWith(".jpg"));
+        System.out.println(Arrays.toString(mapFiles));
+        mapImages = new Image[mapFiles.length];
+        for (int i = 0; i < mapFiles.length; i++)
+        {
+        	mapImages[i] = new Image("file:" + mapFiles[i]);
+        }
+        ObservableList<String> mapStringList = FXCollections.observableArrayList();
+        for (File i : mapFiles)
+        	mapStringList.add(i.getName());
+        mapListView.setItems(mapStringList);
+        mapListView.setOrientation(Orientation.HORIZONTAL);
+        //mapListView.getSelectionModel().getSelectedIndex();
+        
+        //https://stackoverflow.com/questions/25570803/image-in-javafx-listview
+        mapListView.setCellFactory(listView -> new ListCell<String>() {
+		    private ImageView imageView = new ImageView();
+			    @Override
+			    public void updateItem(String fileName, boolean empty) {
+			        super.updateItem(fileName, empty);
+			        if (empty) {
+			            setText(null);
+			            setGraphic(null);
+			        } else {
+			            Image image = fetch(fileName);
+			            imageView.setImage(image);
+			            imageView.setFitHeight(180);
+			            imageView.setPreserveRatio(true);
+			            //https://stackoverflow.com/questions/10949461/javafx-2-click-and-double-click
+			            imageView.setOnMousePressed((MouseEvent e) -> {
+			            	if (e.getButton().equals(MouseButton.PRIMARY) && e.getClickCount() > 1)
+			            	{
+			            		LandingPage.openMapCreator(getSelectedMap());
+			            	}
+			            });
+			            setText(fileName);
+			            setGraphic(imageView);
+			        }
+			    }
+		    });
+    }
+    
+    private Image fetch(String file)
+    {
+    	for (int i = 0; i < mapFiles.length; i++)
+    		if (mapFiles[i].getName().equals(file))
+    			return mapImages[i];
+    	System.out.println("null image: " + file);
+    	return null;
+    }
+    
+    private Image getSelectedMap()
+    {
+    	try
+    	{
+    		return mapImages[mapListView.getSelectionModel().getSelectedIndex()];
+    	}
+    	catch (ArrayIndexOutOfBoundsException e)
+    	{
+    		e.printStackTrace(); //TODO remove
+    		return null;
+    	}
     }
 
-    /*@FXML
-    private void handleButtonAction(ActionEvent event) {
-        label.setText("Hello World!");
-    }*/
+    @FXML
+    private void handlePlayButtonPress(ActionEvent event) {
+        LandingPage.openGame(getSelectedMap());
+    }
+    
+    @FXML
+    private void handleMapCreatorButtonPress(ActionEvent event)
+    {
+    	LandingPage.openMapCreator();
+    }
 }
