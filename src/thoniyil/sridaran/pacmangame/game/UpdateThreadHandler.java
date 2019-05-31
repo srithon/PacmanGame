4f676ecf8269781f23c268c8d74d7b225bb7274e
Sridaran Thoniyil <10015935@sbschools.org>
2019-05-31 09:38:43 -0400

    Un-reverted

--- src/thoniyil/sridaran/pacmangame/game/UpdateThreadHandler.java
+++ src/thoniyil/sridaran/pacmangame/game/UpdateThreadHandler.java
@@ -12,55 +12,61 @@ import thoniyil.sridaran.pacmangame.game.ui.Board;
 public class UpdateThreadHandler
 {
 	private int updateDelay;
-	
+
 	private boolean stopped;
-	
+
 	private static Direction moveChar;
-	
+
 	private Thread t;
-	
+
 	public UpdateThreadHandler(int updatesPerSecond)
 	{
 		// 1000 millis/second
 		updateDelay = 1000 / updatesPerSecond;
-		
+
 		t = new Thread(this::run);
 	}
-	
+
 	public void begin()
 	{
 		t.start();
 	}
-	
+
 	public void run()
 	{
 		Pacman pacman = Board.getPacman();
 		ArrayList<Ghost> ghosts = Board.getGhosts();
-		
+
 		GameController.setNextMoveChar(Direction.randomDirection());
-		
+
 		while (!stopped)
 		{
+			long startTime = System.currentTimeMillis();
+
 			pacman.move();
-			
+
 			for (Ghost g : ghosts)
 			{
 				g.update();
 			}
-			
+
 			Board.refresh();
-			
+
 			try
 			{
-				Thread.sleep(updateDelay);
+				Thread.sleep((updateDelay - (System.currentTimeMillis() - startTime)));
 			}
 			catch (InterruptedException e)
 			{
-				
+				e.printStackTrace();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	public void stop()
 	{
 		stopped = true;
